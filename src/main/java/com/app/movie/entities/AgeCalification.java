fbb764f778da013590d2ca334144a252f29163f6
juan-ariza <juan.ariza.mt@usa.edu.co>
2022-12-01 21:35:08 -0500

    Se corrige la entidad 'Age Calification'

--- src/main/java/com/app/movie/entities/AgeCalification.java
+++ src/main/java/com/app/movie/entities/AgeCalification.java
@@ -9,18 +9,16 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
 
 @Document("agecalification")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class AgeCalification {
+public class AgeCalification implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private String id;
     private String calification;
-
-
-
 }
